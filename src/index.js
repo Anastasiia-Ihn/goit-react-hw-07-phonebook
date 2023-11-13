158d24c2c1a9aadc0c27869e331e927f68c7692d
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-11-13 11:41:30 +0100

    delete Persist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = {
   color: '#485968',
@@ -15,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={{ theme }}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </Provider>
       <GlobalStyle />
     </ThemeProvider>
